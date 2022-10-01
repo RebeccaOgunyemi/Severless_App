6992b77d406f38434b86ee9c66e1a7da6fb40a6d
RebeccaOgunyemi <olufunmilayosmiles@gmail.com>
2022-10-01 20:09:44 +0100

    Serverless_App

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '6gan5tv7kf'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-fvatkt3u.us.auth0.com',
+  clientId: 'NygVzrJXqtjWoXB2sXTmDHcm4sR0AR9P',
+  callbackUrl: 'http://localhost:3000/callback'
+}
